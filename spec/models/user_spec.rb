6e2e462f0b03ff4ece84e46888c9568df520f920
Tamara Orr <tamarakorr@gmail.com>
2014-02-02 22:38:26 +0000

    Improve tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,12 +20,12 @@ describe User do
     before {@user.name = " "}
     it {should_not be_valid}
   end
-  
+
   describe "when email is not present" do
     before {@user.email = " "}
     it { should_not be_valid}
   end
-  
+
   describe "when name is too long" do
     before {@user.name = "a" * 51}
     it { should_not be_valid }
@@ -34,14 +34,14 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-        foo@bar_baz.com foo@bar+baz.com]
+        foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
       end
     end
   end
-  
+
   describe "when email format is valid" do
     it "should be valid" do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
@@ -51,7 +51,17 @@ describe User do
       end
     end
   end
-  
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
